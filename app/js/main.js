811fb650621cc482e0c0acf1cb13678141e95a32
denktag <1deniskri@mail.ru>
2020-09-09 23:46:12 +0300

    added advantages and featured

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,13 @@
 $(function(){
 
+	let mySwiper = new Swiper('.swiper-container', {
+		fade: true,
+		loop: true,
 
+		navigation: {
+			nextEl: '.featured__slider-btn--next',
+			prevEl: '.featured__slider-btn--prev',
+		},
+	})
 
 });
